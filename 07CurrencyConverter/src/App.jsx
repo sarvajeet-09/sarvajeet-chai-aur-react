2b320704d9d6dc14d587c5e42d24d2b9a0e0d59b
sarvajeet-09 <sarvajeetsingh771@gmail.com>
2025-01-07 23:47:46 +0530

    tuesday-push

--- 07CurrencyConverter/src/App.jsx
+++ 07CurrencyConverter/src/App.jsx
@@ -2,9 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+const[amount,setAmount] = useState(0)
+const[from, setFrom] = useState("usd")
+const[to, setTo] = useState("inr")
+const[convertedAmount, setConvertedAmount ] = useState(0)
+
+
+const currencyInfo = useCurrencyInfo(from)
+
+const options = Object.keys(currencyInfo)
+const swap = () =>{
+  setFrom(to)
+  setTo(from)
+  setConvertedAmount(amount)
+  setAmount(convertedAmount)
+}
+const covert = () => {
+  setConvertedAmount(amount * currencyInfo[to])
+}
 
   return (
     <>
---
+++ 07CurrencyConverter/src/components/index.js
@@ -0,0 +1,3 @@
+import InputBox from './InpuBoxt'
+
+export{InputBox}
