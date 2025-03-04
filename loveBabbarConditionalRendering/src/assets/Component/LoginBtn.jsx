1bf7a4eb5d8adf5768436f9b71bd26e7ec4644a2
sarvajeet-09 <sarvajeetsingh771@gmail.com>
2025-03-04 23:54:10 +0530

    Update LoginBtn.jsx

--- loveBabbarConditionalRendering/src/assets/Component/LoginBtn.jsx
+++ loveBabbarConditionalRendering/src/assets/Component/LoginBtn.jsx
@@ -1,36 +1,35 @@
 import React from 'react'
 import './LoginBtn.css'
 const LoginBtn = (props) => {
-  const verify = (event) =>{
-    const email = event.target.email.value.trim().toLowerCase()
-    const password = event.target.password.value.trim().toLowerCase()
 
+
+
+  const verify = (event) =>{
+    const email = event.target.email.value.trim()
+    const password = event.target.password.value.trim()
     const mymail = 'sarvajeetsingh771@gmail.com'
     const mypass = 'shubham@123'
 
     if(email === mymail && password === mypass){
-
+      props.updateBtn(true)
+      }
+    else{
+      props.updateBtn(false)
+      alert("enter a valid email and password")
     }
+    event.preventDefault()
   } 
   
   return (
     <>
-
-    
-
       <div className='login-page-css'>
-        <form action="#">
+        <form action="#" onSubmit={verify}>
        <h1> User Login</h1>
         
       <input type="text" placeholder='Enter Email' name='email' />
       <input type="password" placeholder='password' name = 'password' />
 
-        <button type="submit" 
-         onClick={verify}
-        // onClick={() => {
-        //   props.updateBtn(true)
-        // }}
-        >LOG IN</button>
+        <button type="submit">LOG IN</button>
         </form>
       </div>
     </>
