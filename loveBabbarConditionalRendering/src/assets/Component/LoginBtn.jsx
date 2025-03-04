f26cb098ab55c938fdc5f5fe5691a63f8973965b
sarvajeet-09 <sarvajeetsingh771@gmail.com>
2025-03-04 23:39:08 +0530

    commit

--- loveBabbarConditionalRendering/src/assets/Component/LoginBtn.jsx
+++ loveBabbarConditionalRendering/src/assets/Component/LoginBtn.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
-
+import './LoginBtn.css'
 const LoginBtn = (props) => {
-  return (
-    <div>
-        <button type="button" onClick={() =>{
+  const verify = (event) =>{
+    const email = event.target.email.value.trim().toLowerCase()
+    const password = event.target.password.value.trim().toLowerCase()
 
-         props.updateBtn(true)
-         }}
-         >LOG IN</button>
-    </div>
+    const mymail = 'sarvajeetsingh771@gmail.com'
+    const mypass = 'shubham@123'
+
+    if(email === mymail && password === mypass){
+
+    }
+  } 
+  
+  return (
+    <>
+
+    
+
+      <div className='login-page-css'>
+        <form action="#">
+       <h1> User Login</h1>
+        
+      <input type="text" placeholder='Enter Email' name='email' />
+      <input type="password" placeholder='password' name = 'password' />
+
+        <button type="submit" 
+         onClick={verify}
+        // onClick={() => {
+        //   props.updateBtn(true)
+        // }}
+        >LOG IN</button>
+        </form>
+      </div>
+    </>
   )
 }
 export default LoginBtn
--- loveBabbarConditionalRendering/src/assets/Component/LogoutBtn.jsx
+++ loveBabbarConditionalRendering/src/assets/Component/LogoutBtn.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import './LogoutBtn.css'
 
 const LogoutBtn = (props) => {
+  
+
   return (
-    <div>
+    <>
+    <div className='logout-page-css'>
+      <div className='information'>
+      <h1>Name:<span> Sarvajeet Singh</span></h1>
+     <h2>Designation: <span>Frontend Developer</span></h2>
+     <h2>Qualification: <span>B.tech(CS)</span></h2>
       <button
         type="button"
+       
         onClick={() => {
           props.updateBtn(false); // Correctly call the function with `false`
         }}
       >
         LOG OUT
       </button>
-    </div>
+      </div>
+    </div></>
   );
 };
 
