68b224ca984036a4b00ee1a7c3951e338761ec12
sarvajeet-09 <sarvajeetsingh771@gmail.com>
2025-03-06 23:58:35 +0530

    Update App.jsx

--- 01vitebasicreact/src/App.jsx
+++ 01vitebasicreact/src/App.jsx
@@ -1,10 +1,28 @@
-import Chai from "./Shubham";
+
+import React from "react"
+import { useEffect, useState } from "react"
 function App() {
+const [product, setProduct] = useState(null)
+  const myEffect = useEffect(()  => {
+   const fecthdata = async () =>{
+    try{
+      const response = await fetch("https://fakestoreapi.com/products/1")
+      const result = await response.json()
+      console.log(result)
+      setProduct(result)
+    }
+    catch(err){
+      console.log(err)
+    }
+   }
+   fecthdata();
+  },[])
+
 
   return (
     <>
-    <Chai />
-    <h1>This is My First react app using vite + react</h1>
+    <h1>{product.title}</h1>
+    {/* <button onClick={myEffect}>click here</button> */}
     </>
   )
 }
