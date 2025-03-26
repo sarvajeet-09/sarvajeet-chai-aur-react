dcf432343d97121b54a5ad04f68a0137416bc040
sarvajeet-09 <sarvajeetsingh771@gmail.com>
2025-03-26 23:17:32 +0530

    commit

--- e-commerce-product-catalogue/src/components/ProductDetail.jsx
+++ e-commerce-product-catalogue/src/components/ProductDetail.jsx
@@ -1,10 +1,22 @@
 import { React, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { items } from './Data'
+import './ProductDetail.css'
+import './Product.css'
 
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState([])
+  const [related, setRelated] = useState([])
+
+// for scrolling the window after click
+   function scrollClick(){
+    window.scroll({
+      top:0,
+      behavior:"smooth"
+    })
+   }
+
 
   useEffect(() => {
     const filterProduct = items.filter((product) => {
@@ -12,16 +24,76 @@ const ProductDetail = () => {
     })
     console.log(filterProduct)
     setProduct(filterProduct[0])
-  }, [id])
+
+    const relatedProducts = items.filter((P) => P.category === product.category)
+    // console.log(relatedProducts.id)
+    setRelated(relatedProducts)
+
+  }, [id, product.category])
   return (
-    <div>
-      <div>
-        <div className="img">
-          <h2>{product.category}</h2>
-          <img src={product.imgSrc} alt="" />
+    <>
+      {/* main product displaying start */}
+
+      <div className="container">
+        <div className="product-detail-div">
+          <div className="row">
+            <div className="col-lg-5">
+              <div className="img">
+                <img src={product.imgSrc} className="img-fluid" alt="" />
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="details">
+                <h3>{product.title}</h3>
+                <p>{product.description}</p>
+                <h4> <i className="fa fa-indian-rupee-sign"></i> {product.price}</h4>
+
+                <div className="button-div">
+                  <button className="btn1">
+                    Add To Cart <i className="fa fa-cart-shopping"></i>
+                  </button>
+                  <button className="btn2">
+                    Buy Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* main product displaying end */}
+
+      {/* related product start */}
+
+      <div className="container">
+        <div className="row">
+          {
+            related.map((product) => {
+              return (
+                <div className="col-lg-4 mb-4">
+                  <Link to={`/product/${product.id}`} key={product.id} onClick={scrollClick} className="main-card">
+                    <div className="pro-card">
+                      <h3>{product.title}</h3>
+                      <img src={product.imgSrc} className='img-fluid img' alt="" />
+                      <p>{product.description}</p>
+                    </div>
+                    <div className="price-about">
+                      <button type='button' className='btn'><i className='fa fa-indian-rupee-sign'></i> {product.price}</button>
+                      <button type='button' className='btn '>Add to Cart <i className='fa fa-cart-shopping'></i></button>
+                    </div>
+                  </Link>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      {/* related product end */}
+
+
+
+    </>
   )
 }
 
